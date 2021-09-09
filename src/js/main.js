ce197de5a2801ebff5a10911fe961c8042bb85d3
AndreaAguado <andrea95am@gmail.com>
2021-09-09 18:50:18 +0200

    bring to js html elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const input = document.querySelector('.js_input');
+
+const list = document.querySelector('.js_list');
+
+const button = document.querySelector('.js_submit_button');
+
+
+
