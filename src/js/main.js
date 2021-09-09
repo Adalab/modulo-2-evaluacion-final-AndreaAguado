b3ea8bdb341636e9e0bb07fd25f1730681e8191a
AndreaAguado <andrea95am@gmail.com>
2021-09-09 19:17:03 +0200

    add function to customize url

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,23 @@ const list = document.querySelector('.js_list');
 
 const button = document.querySelector('.js_submit_button');
 
+let url;
+function urlMaker(){
+  url='//api.tvmaze.com/search/shows?q=';
+  url += input.value;
+  console.log(input.value);
+  console.log(url);
+  return url;
+}
 
+// function requestToAPI(){
+//     url=urlMaker();
+//   fetch()
+// }
 
+function handleInput(){
+  console.log(input.value);
+  urlMaker();
+}
+
+input.addEventListener('change',handleInput);
