6f2f5b5c76ee22366b235e874523d1d23dd20cfa
AndreaAguado <andrea95am@gmail.com>
2021-09-09 19:32:40 +0200

    insert data received into array

--- src/js/main.js
+++ src/js/main.js
@@ -15,14 +15,25 @@ function urlMaker(){
   return url;
 }
 
+let shows = [];
 function requestToAPI(){
   url=urlMaker();
   fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        shows[i]=data[i];
+      }
     });
+  console.log(shows);
 }
+
+// let html = '';
+// function displayResults(){
+//   html += `<li class="list_item js_list_item">`;
+// }
+
 
 function handleButton(ev){
   ev.preventDefault();
