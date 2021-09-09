d32892d532857243d5b41861608dc94ce4061b64
AndreaAguado <andrea95am@gmail.com>
2021-09-09 21:26:26 +0200

    delete unused functions

--- src/js/main.js
+++ src/js/main.js
@@ -27,45 +27,24 @@ function requestToAPI(){
         images[i]=shows[i].image;
         showTitles[i]=shows[i].name;
       }
-    console.log(shows);
-    console.log(images);
-    console.log(showTitles);
-    displayResults();
+      console.log(shows);
+      console.log(images);
+      console.log(showTitles);
+      displayResults();
     });
- 
-//   console.log(shows[0].show);
 }
 
 
 let html = '';
 function displayResults(){
-   for (let i = 0; i < shows.length; i++) {
+  for (let i = 0; i < shows.length; i++) {
     html += `<li class="list_item js_list_item">`;
     html += `<img class="img" src=${images[i].medium} alt="movie cover">`;
     html += `  <h2 class="movie_title">${showTitles[i]}</h2>`;
     html += `<h2 class="movie_title">Movie Title</h2>`;
     html += `</li>`;
     list.innerHTML = html;
-   }
-
-    
-
-}
-
-let showInfo = [];
-function extractShowInfo(){
-//   for (let i = 0; i < shows.length; i++) {
-//     showInfo.push(shows[i].show);
-//     // showInfo[i]=shows[i].show;
-//   }
-
-//   showInfo = shows[0].show;
-// for (const show of shows) {
-//     showInfo.push(show.show);
-// }
-  console.log(showInfo);
-
-  return showInfo;
+  }
 }
 
 
@@ -73,9 +52,6 @@ function handleButton(ev){
   ev.preventDefault();
   urlMaker();
   requestToAPI();
-//   let info = extractShowInfo();
-//   displayResults(info);
-
 }
 
 
