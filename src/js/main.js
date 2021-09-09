b268ab72012e746dc9b743a5f306862099fde2a8
AndreaAguado <andrea95am@gmail.com>
2021-09-09 21:13:02 +0200

    leave score property out of shows array

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 const input = document.querySelector('.js_input');
 
 const list = document.querySelector('.js_list');
@@ -20,24 +18,46 @@ function requestToAPI(){
   url=urlMaker();
   fetch(url)
     .then(response => response.json())
-    .then(data => {
+    .then((data) => {
       console.log(data);
       for (let i = 0; i < data.length; i++) {
-        shows[i]=data[i];
+        shows[i]=data[i].show;
       }
+    console.log(shows);
+    displayResults();
     });
-  console.log(shows);
+ 
+//   console.log(shows[0].show);
 }
 
 
 let html = '';
 function displayResults(){
-  html += `<li class="list_item js_list_item">`;
-  //   html += `<img class="img" src="${}" alt="movie cover">`;
-  //   html += `  <h2 class="movie_title">${}</h2>`;
-  html += `<h2 class="movie_title">Movie Title</h2>`;
-  html += `</li>`;
-  list.innerHTML = html;
+    for (const show of shows) {
+        html += `<li class="list_item js_list_item">`;
+        html += `<img class="img" src="${show.name}" alt="movie cover">`;
+        // html += `  <h2 class="movie_title">${}</h2>`;
+        html += `<h2 class="movie_title">Movie Title</h2>`;
+        html += `</li>`;
+        list.innerHTML = html;
+    }
+
+}
+
+let showInfo = [];
+function extractShowInfo(){
+//   for (let i = 0; i < shows.length; i++) {
+//     showInfo.push(shows[i].show);
+//     // showInfo[i]=shows[i].show;
+//   }
+
+//   showInfo = shows[0].show;
+// for (const show of shows) {
+//     showInfo.push(show.show);
+// }
+  console.log(showInfo);
+
+  return showInfo;
 }
 
 
@@ -45,7 +65,9 @@ function handleButton(ev){
   ev.preventDefault();
   urlMaker();
   requestToAPI();
-  displayResults();
+//   let info = extractShowInfo();
+//   displayResults(info);
+
 }
 
 
